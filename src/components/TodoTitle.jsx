6129ad94f92ef13ffd73dc2d478ca16089685e58
Rohan Adhikari <adhikarirohan249@gmail.com>
2024-01-22 14:42:44 +0545

    simple interfcae built

---
+++ src/components/TodoTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoTitle = () => {
+  return (
+    <div className="pt-5">
+      <h3>Todo List</h3>
+      <button className="btn btn-primary mt-4">Create new</button>
+    </div>
+  );
+};
+
+export default TodoTitle;
